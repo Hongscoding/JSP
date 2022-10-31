5f150adb3ddc9f8df079f010c0d7ad5017e0f481
alswns3906 <alswns3906@naver.com>
2022-10-31 11:21:58 +0900

    글 보기/ 파일 다운로드 구현

--- JBoard1/src/main/java/kr/co/jboard1/db/sql.java
+++ JBoard1/src/main/java/kr/co/jboard1/db/sql.java
@@ -1,47 +1,70 @@
 package kr.co.jboard1.db;
 
 public class sql {
-
+	
 	// user
-	public static final String INSERT_USER 		 ="INSERT INTO `board_user` set "
-												 + "`uid`=?, "	
-												 + "`pass`=SHA2(?, 256),"
-												 + "`name`=?, "
-												 + "`nick`=?, "
-												 + "`email`=?, "
-												 + "`hp`=?, "
-												 + "`zip`=?,"
-												 + "`addr1`=?,"
-												 + "`addr2`=?,"
-												 + "`regip`=?,"
-												 + "`rdate`=now()";
+	public static final String INSERT_USER      = "insert into `board_user` set "
+												+ "`uid`=?,"
+												+ "`pass`=SHA2(?, 256),"
+												+ "`name`=?,"
+												+ "`nick`=?,"
+												+ "`email`=?,"
+												+ "`hp`=?,"
+												+ "`zip`=?,"
+												+ "`addr1`=?,"
+												+ "`addr2`=?,"
+												+ "`regip`=?,"
+												+ "`rdate`=NOW()";
 	
-	public static final String SELECT_USER 		 ="select * from `board_user` where `uid`=? and `pass`=SHA2(?, 256)";
-	public static final String SELECT_COUNT_UID  ="select count(`uid`) from `board_user` where `uid`=?";
-	public static final String SELECT_COUNT_NICK ="select count(`nick`) from `board_user` where `nick`";
-	public static final String SELECT_TERMS 	 ="select * from `board_terms`";
+	public static final String SELECT_USER       = "select * from `board_user` where `uid`=? and `pass`=SHA2(?, 256)";
+	public static final String SELECT_COUNT_UID  = "select count(`uid`) from `board_user` where `uid`=?";
+	public static final String SELECT_COUNT_NICK = "select count(`nick`) from `board_user` where `nick`=?";
+	public static final String SELECT_TERMS      = "select * from `board_terms`";
 	
+	// board
+	public static final String INSERT_ARTICLE = "insert into `board_article` set "
+												+ "`title`=?,"
+												+ "`content`=?,"
+												+ "`file`=?,"
+												+ "`uid`=?,"
+												+ "`regip`=?,"
+												+ "`rdate`=NOW()";
 	
+	public static final String INSERT_FILE = "insert into `board_file` set "
+											+ "`parent`=?,"
+											+ "`newName`=?,"
+											+ "`oriName`=?,"
+											+ "`rdate`=NOW()";
 	
-	//board
-	public static final String INSERT_ARTICLE 	 ="insert into `board_article` set "
-												 + "`title`=?,"
-												 + "`content`=?,"
-												 + "`file`=?,"
-												 + "`uid`=?,"
-												 + "`regip`=?,"
-												 + "`rdate`=now()";
-	public static final String INSERT_FILE		 ="insert into `board_file` set "
-												 +"`parent`=?, "
-												 +"`newName`=?, "
-												 +"`oriName`=?, "
-												 +"`rdate`=now()";
-	public static final String SELECT_MAX_NO ="select max(`no`) from `board_article`";
-	public static final String SELECT_COUNT_TOTAL ="SELECT COUNT(`no`) FROM `board_article`";
-	public static final String SELECT_ARTICLES = "SELECT a.*, b.`nick` FROM `board_article` AS a  "
+	public static final String SELECT_MAX_NO = "select max(`no`) from `board_article`";
+	public static final String SELECT_COUNT_TOTAL = "SELECT COUNT(`no`) FROM `board_article`";	
+	public static final String SELECT_ARTICLES = "SELECT a.*, b.`nick` FROM `board_article` AS a "
 												+ "JOIN `board_user` AS b ON a.uid = b.uid "
 												+ "ORDER BY `no` DESC "
-												+ "LIMIT ?,10 ";
+												+ "LIMIT ?, 10";
 	
+	public static final String SELECT_ARTICLE = "SELECT a.*, b.`fno`, b.`oriName`, b.`download` "
+												+ "FROM `board_article` AS a "
+												+ "LEFT JOIN `board_file` AS b "
+												+ "ON	a.`no` = b.`parent` "
+												+ "WHERE `no`=?";
+	
+	public static final String SELECT_FILE ="select * from `board_file` where `fno`=?";
+	
+	public static final String UPDATE_ARTICLE_HIT = "UPDATE `board_article` SET `hit` = `hit` + 1 WHERE `no`=?";
+
+	public static final String UPDATE_FILE_DOWNLOAD = "UPDATE `board_file` SET `download` = `download` + 1 WHERE `fno`=?";
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
--- JBoard1/src/main/webapp/view.jsp
+++ JBoard1/src/main/webapp/view.jsp
@@ -1,19 +1,40 @@
+<%@page import="kr.co.jboard1.dao.ArticleDAO"%>
+<%@page import="kr.co.jboard1.bean.ArticleBean"%>
+<%@page import="java.sql.ResultSet"%>
+<%@page import="java.sql.PreparedStatement"%>
+<%@page import="java.sql.Connection"%>
+<%@page import="kr.co.jboard1.db.DBCP"%>
+<%@page import="kr.co.jboard1.db.sql"%>
 <%@ page contentType="text/html;charset=UTF-8" pageEncoding="UTF-8"%>
+<%
+	request.setCharacterEncoding("UTF-8");
+	String no = request.getParameter("no");
+	// DAO 객체 가져오기
+	ArticleDAO dao = ArticleDAO.getInstance();
+	
+	// 글 조회수 카운트 +1
+	dao.updateArticleHit(no);
+	
+	// 글 가져오기
+	ArticleBean article = dao.selectArticle(no);
+%>
 <%@ include file="./_header.jsp" %>
 <main id="board" class="view">
     <table>
         <caption>글보기</caption>
         <tr>
             <th>제목</th>
-            <td><input type="text" name="title" value="제목입니다." readonly></td>
+            <td><input type="text" name="title" value="<%= article.getTitle() %>" readonly/></td>
         </tr>
+        <% if(article.getFile() > 0){ %>
         <tr>
-            <th>첨부파일</th>
-            <td><a href="#">2022년 상반기 매출자료.xls</a><span>7</span>회 다운로드</td>
+            <th>파일</th>
+            <td><a href="/JBoard1/proc/download.jsp?fno=<%= article.getFno() %>"><%= article.getOriName() %></a>&nbsp;<span><%= article.getDownload() %></span>회 다운로드</td>
         </tr>
+        <% } %>
         <tr>
             <th>내용</th>
-            <td><textarea name="content" readonly>내용 샘플 입니다.</textarea></td>
+            <td><textarea name="content" readonly><%= article.getContent() %></textarea></td>
         </tr>
     </table>
 
@@ -22,29 +43,30 @@
         <a href="/JBoard1/modify.jsp" class="btn btnModify">수정</a>
         <a href="/JBoard1/list.jsp" class="btn btnList">목록</a>
     </div>
-    <!-- 댓글 목록 -->
+
+    <!-- 댓글목록 -->
     <section class="commentList">
         <h3>댓글목록</h3>
         <article>
             <span class="nick">길동이</span>
-            <span class="date">20-05-13</span>
+            <span class="date">20-05-13</span>                    
             <p class="content">댓글 샘플입니다.</p>
             <div>
-                <a href="#" class="Remove">삭제</a>
-                <a href="#" class="Modify">수정</a>
+                <a href="#" class="remove">삭제</a>
+                <a href="#" class="modify">수정</a>
             </div>
-        </article>
+        </article>                
         <p class="empty">등록된 댓글이 없습니다.</p>
     </section>
 
-    <!-- 댓글 쓰기 -->
+    <!-- 댓글쓰기 -->
     <section class="commentForm">
         <h3>댓글쓰기</h3>
         <form action="#">
             <textarea name="content" placeholder="댓글내용 입력"></textarea>
             <div>
                 <a href="#" class="btn btnCancel">취소</a>
-                <input type="submit" class="btn btnComplete" value="작성완료">
+                <input type="submit" class="btn btnComplete" value="작성완료"/>
             </div>
         </form>
     </section>
